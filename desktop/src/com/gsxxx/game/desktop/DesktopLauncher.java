313fd20e938eed67ef7becfb713cb7f4a2906bbc
GSxxx <prenc@student.agh.edu.pl>
2017-12-30 01:05:55 +0100

    small desktop launcher changes

--- desktop/src/com/gsxxx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/gsxxx/game/desktop/DesktopLauncher.java
@@ -5,8 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gsxxx.game.MammothGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MammothGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new MammothGame(), config);
+
+        //target resolution of app
+        config.width = 1920;
+        config.height = 1080;
+    }
 }
