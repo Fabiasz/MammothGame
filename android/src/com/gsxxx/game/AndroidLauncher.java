28edb437e6927d2204568823ab9afccc0c06a58b
GSxxx <prenc@student.agh.edu.pl>
2017-12-30 01:18:04 +0100

    hiding soft keys

--- android/src/com/gsxxx/game/AndroidLauncher.java
+++ android/src/com/gsxxx/game/AndroidLauncher.java
@@ -1,20 +1,19 @@
 package com.gsxxx.game;
 
 import android.os.Bundle;
-
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.View;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.gsxxx.game.MammothGame;
+
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MammothGame(), config);
-	}
+
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new MammothGame(), config);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
 }
