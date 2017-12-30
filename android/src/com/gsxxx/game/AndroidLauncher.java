0c0b9bcc716303c37316a64b3111f16ea49948e0
GSxxx <prenc@student.agh.edu.pl>
2017-12-30 01:06:43 +0100

    small android launcher changes

--- android/src/com/gsxxx/game/AndroidLauncher.java
+++ android/src/com/gsxxx/game/AndroidLauncher.java
@@ -2,6 +2,8 @@ package com.gsxxx.game;
 
 import android.os.Bundle;
 
+import android.view.Window;
+import android.view.WindowManager;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.gsxxx.game.MammothGame;
@@ -10,6 +12,8 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MammothGame(), config);
 	}
