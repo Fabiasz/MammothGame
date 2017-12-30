18c5774adf0eca708225f937313e0667b688d7ef
GSxxx <prenc@student.agh.edu.pl>
2017-12-30 01:07:23 +0100

    adding mammoth and background to core

--- core/src/com/gsxxx/game/MammothGame.java
+++ core/src/com/gsxxx/game/MammothGame.java
@@ -2,32 +2,33 @@ package com.gsxxx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
 
 public class MammothGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    EndlessScrollingBackground background;
+    Mammoth mammoth;
+
+    @Override
+    public void create() {
+        background = new EndlessScrollingBackground();
+        mammoth = new Mammoth();
+    }
+
+    @Override
+    public void render() {
+        background.render();
+        mammoth.render();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        mammoth.dispose();
+    }
 }
