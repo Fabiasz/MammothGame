eb2bd7cba69a49a7b2321e82bbc0b8fd5baaff42
GSxxx <prenc@student.agh.edu.pl>
2018-01-01 19:58:16 +0100

    fixing fullscreen mode

--- android/src/com/gsxxx/game/AndroidLauncher.java
+++ android/src/com/gsxxx/game/AndroidLauncher.java
@@ -1,5 +1,7 @@
 package com.gsxxx.game;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -14,6 +16,19 @@ public class AndroidLauncher extends AndroidApplication {
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         initialize(new MammothGame(), config);
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            hideVirtualButtons();
+        }
+    }
+
+    @TargetApi(19)
+    private void hideVirtualButtons() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
