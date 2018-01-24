46c7ede0177069942c0ab0ec6afff55415b80ab2
GSxxx <prenc@student.agh.edu.pl>
2018-01-24 22:16:45 +0100

    fixing contact listener stick joints were created every iteration of game loop

--- core/src/com/gsxxx/game/MyContactListener.java
+++ core/src/com/gsxxx/game/MyContactListener.java
@@ -15,34 +15,28 @@ public class MyContactListener implements ContactListener {
 
     @Override
     public void beginContact(Contact contact) {
-
-    }
-
-    @Override
-    public void endContact(Contact contact) {
-
-    }
-
-    @Override
-    public void preSolve(Contact contact, Manifold oldManifold) {
-
-    }
-
-    @Override
-    public void postSolve(Contact contact, ContactImpulse impulse) {
         Fixture fixtureA = contact.getFixtureA();
         Fixture fixtureB = contact.getFixtureB();
         Body bodyA = fixtureA.getBody();
         Body bodyB = fixtureB.getBody();
-
 
         if (bodyA.getUserData().equals("mammoth") && bodyB.getUserData().equals("spearHead")) {
             thingsToStick.add(new StickInfo(bodyA, bodyB, contact.getWorldManifold().getPoints()));
         } else if (bodyA.getUserData().equals("spearHead") && bodyB.getUserData().equals("mammoth")) {
             thingsToStick.add(new StickInfo(bodyB, bodyA, contact.getWorldManifold().getPoints()));
         }
+    }
 
+    @Override
+    public void endContact(Contact contact) {
+    }
 
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
     }
 
     class StickInfo{
